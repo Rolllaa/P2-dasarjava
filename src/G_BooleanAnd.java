def12b79585f9bd12e3bf3327aeafb1e16600bc9
rol <shahrulmubarak2020@gmail.com>
2022-03-27 18:46:30 +0700

    _8_MenggunakanOperatorBooleanAnd

---
+++ src/G_BooleanAnd.java
@@ -0,0 +1,12 @@
+public class G_BooleanAnd {
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+        if ((a<2) & (b++< 10)){
+            b+=2;
+
+        }
+        System.out.println(b);
+
+    }
+}
+
