741510f2cdbd09827b26eea08b2951ebfb68cba6
rol <shahrulmubarak2020@gmail.com>
2022-03-27 18:34:55 +0700

    3_MelakukanIncrementDanDecremenNilai

---
+++ src/C_incDec.java
@@ -0,0 +1,11 @@
+public class C_incDec {
+        public static void main(String[] args) {
+            int x = 8, y = 13;
+            System.out.println("x=" + x);
+            System.out.println("y =" + y);
+            System.out.println("x=" + ++x);
+            System.out.println("y=" + y++);
+            System.out.println("x=" + x--);
+            System.out.println("y=" + --y);
+        }
+    }
